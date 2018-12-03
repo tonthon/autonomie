369821afd5f45e8794afbfb8beac9f757196b754
G.Tjebbes <g.t@majerti.fr>
2018-12-03 19:08:29 +0100

    Fix #955 : on masque le champ "Taux de Responsabilité Civile Professionnel"

--- autonomie/forms/company.py
+++ autonomie/forms/company.py
@@ -209,23 +209,23 @@ comptabilité",
     #     missing="",
     # )
 
-    custom_insurance_rate = colander.SchemaNode(
-            QuantityType(),
-            widget=deform.widget.TextInputWidget(
-                input_append="%",
-                css_class="col-md-1"
-                ),
-            validator=colander.Range(
-                min=0,
-                max=100,
-                min_err=u"Veuillez fournir un nombre supérieur à 0",
-                max_err=u"Veuillez fournir un nombre inférieur à 100"
-            ),
-            title=u"Taux de Responsabilité Civile Professionnel",
-            missing=colander.drop,
-            description=u"Pourcentage du taux d'assurance professionnelle "
-                        u"de cette entreprise dans la CAE",
-    )
+    # custom_insurance_rate = colander.SchemaNode(
+    #         QuantityType(),
+    #         widget=deform.widget.TextInputWidget(
+    #             input_append="%",
+    #             css_class="col-md-1"
+    #             ),
+    #         validator=colander.Range(
+    #             min=0,
+    #             max=100,
+    #             min_err=u"Veuillez fournir un nombre supérieur à 0",
+    #             max_err=u"Veuillez fournir un nombre inférieur à 100"
+    #         ),
+    #         title=u"Taux de Responsabilité Civile Professionnel",
+    #         missing=colander.drop,
+    #         description=u"Pourcentage du taux d'assurance professionnelle "
+    #                     u"de cette entreprise dans la CAE",
+    # )
 
     contribution = colander.SchemaNode(
             QuantityType(),
